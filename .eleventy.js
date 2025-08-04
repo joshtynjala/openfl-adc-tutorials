dc6173e427fcb8dcc8562acaf5cf3d94a5091012
Josh Tynjala <joshtynjala@bowlerhat.dev>
2025-08-04 15:41:04 -0700

    path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -4,19 +4,16 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const pluginToc = require("eleventy-plugin-toc");
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addGlobalData("baseURL", "https://joshtynjala.github.io/openfl-adc-tutorials/");
+module.exports = async function (eleventyConfig) {
+  const { HtmlBasePlugin } = await import("@11ty/eleventy");
+
   eleventyConfig.addGlobalData("siteTitle", "OpenFL Tutorials");
   eleventyConfig.addGlobalData("layout", "article.html");
   eleventyConfig.addPassthroughCopy("**/*.png");
   eleventyConfig.addPassthroughCopy("**/*.jpg");
   eleventyConfig.addPassthroughCopy("**/*.gif");
-  eleventyConfig.addPassthroughCopy({
-    // "static/img": "img",
-    // "static/fnt": "fnt",
-    // "static/js": "js",
-  });
   eleventyConfig.addPlugin(pluginToc);
+  eleventyConfig.addPlugin(HtmlBasePlugin);
   eleventyConfig.setLibrary(
     "md",
     markdownIt({ html: true }).use(markdownItAnchor)
@@ -26,63 +23,80 @@ module.exports = function (eleventyConfig) {
     console.log(value, typeof value);
     return value;
   });
-  eleventyConfig.addFilter("extract_title", (content) => {
+  eleventyConfig.addFilter("extract_title", /** @param content {string} */ (content) => {
     const match = content.match(/<h1[^>]*>(.*?)<\/h1>/i);
     return match ? match[1] : "";
   });
-  eleventyConfig.addFilter("omit_title", (content) => {
+  eleventyConfig.addFilter("omit_title", /** @param content {string} */ (content) => {
     const match = content.match(/<h1[^>]*>.*?<\/h1>/i);
     return match ? content.substring(match[0].length) : content;
   });
   eleventyConfig.addTemplateFormats("css");
   eleventyConfig.addExtension("css", {
     outputFileExtension: "css",
-    compile: async function (content) {
+    compile: /** @param content {string} */ async function (content) {
       return async () => {
         return new CleanCSS({}).minify(content).styles;
       };
     },
   });
-  eleventyConfig.addTransform("rewritedotmd", function (content) {
+  eleventyConfig.addTransform("rewritedotmd", /** @param content {string} */ function (content) {
     if (
       this.inputPath &&
-      this.inputPath.startsWith("./") &&
       this.inputPath.endsWith(".md") &&
       this.outputPath &&
       this.outputPath.endsWith(".html")
     ) {
-      const lastSlashIndex = this.inputPath.lastIndexOf("/");
-      const startURL = this.inputPath.substring(1, lastSlashIndex + 1);
-      const lastLastSlashIndex = this.inputPath.lastIndexOf(
+    const pathPrefix = module.exports.config?.pathPrefix ?? "/";
+      const firstLastIndex = this.inputPath.endsWith("/index.md") ? this.url.length : this.url.lastIndexOf("/");
+      const lastSlashIndex = this.url.lastIndexOf(
+        "/",
+        firstLastIndex - 1
+      );
+      const startURL = pathPrefix + this.url.substring(1, lastSlashIndex + 1);
+      const lastLastSlashIndex = this.url.lastIndexOf(
         "/",
         lastSlashIndex - 1
       );
       const startURLParent =
         lastLastSlashIndex !== -1
-          ? this.inputPath.substring(1, lastLastSlashIndex + 1)
+          ? pathPrefix + this.url.substring(1, lastLastSlashIndex + 1)
           : startURL;
+    console.warn(startURL);
+    console.warn(startURLParent);
       return content
+        // doesn't start with ./ or ../
         .replace(
-          /<a( class="[\w\-]+")? href="\.\/([\w\-\/]+)\.md(#[\w+\-]+)?"/g,
+          /<a( class="[\w\-]+")? href="(\w[\w\-\/]+)\.md(#[\w+\-]+)?"/g,
           `<a$1 href="${startURL}$2/$3"`
         )
         .replace(
-          /<a( class="[\w\-]+")? href="\.\.\/([\w\-\/]+)\.md(#[\w+\-]+)?"/g,
+          /<img( class="[\w\-]+")? src="(\w[\w\-\/]+)\.(png|jpg|jpeg|gif|webp|svg)"/g,
+          `<img$1 src="${startURL}$2.$3"`
+        )
+        // starts with ./
+        .replace(
+          /<a( class="[\w\-]+")? href="\.\/(\w[\w\-\/]+)\.md(#[\w+\-]+)?"/g,
+          `<a$1 href="${startURL}$2/$3"`
+        )
+        .replace(
+          /<img( class="[\w\-]+")? src="\.\/(\w[\w\-\/]+)\.(png|jpg|jpeg|gif|webp|svg)"/g,
+          `<img$1 src="${startURL}$2.$3"`
+        )
+        // starts with ../
+        .replace(
+          /<a( class="[\w\-]+")? href="\.\.\/(\w[\w\-\/]+)\.md(#[\w+\-]+)?"/g,
           `<a$1 href="${startURLParent}$2/$3"`
         )
         .replace(
-          /<img( class="[\w\-]+")? src="\.\/([\w\-\/]+)\.(png|jpg|jpeg|gif|webp|svg)"/g,
-          `<img$1 src="${startURL}$2.$3"`
-        )
-        .replace(
-          /<img( class="[\w\-]+")? src="\.\.\/([\w\-\/]+)\.(png|jpg|jpeg|gif|webp|svg)"/g,
+          /<img( class="[\w\-]+")? src="\.\.\/(\w[\w\-\/]+)\.(png|jpg|jpeg|gif|webp|svg)"/g,
           `<img$1 src="${startURLParent}$2.$3"`
         )
         .replace("/index/", "/");
     }
     return content;
   });
-  eleventyConfig.addTransform("htmlmin", function (content) {
+  eleventyConfig.addTransform("htmlmin", /** @param content {string} */ function (content) {
     if (this.outputPath && this.outputPath.endsWith(".html")) {
       return htmlmin.minify(content, {
         useShortDoctype: true,
@@ -95,3 +109,7 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 };
+
+module.exports.config = {
+	pathPrefix: "/openfl-adc-tutorials/",
+}
